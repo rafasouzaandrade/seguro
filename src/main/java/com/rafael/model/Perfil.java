695a6ab57ecd954a604ef8b3ca78ccb41c41d54c
Rafael Andrade <rafael.andrade@jexperts.com.br>
2014-09-29 15:46:46 -0300

    JAAS

---
+++ src/main/java/com/rafael/model/Perfil.java
@@ -0,0 +1,37 @@
+package com.rafael.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "perfil")
+public class Perfil {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	@NotNull
+	@NotEmpty
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ src/main/java/com/rafael/model/Usuario.java
@@ -0,0 +1,82 @@
+package com.rafael.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "usuario", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class Usuario {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@NotNull
+	@NotEmpty
+	private String nome;
+
+	@NotNull
+	@NotEmpty
+	@Email
+	private String email;
+
+	@NotNull
+	@NotEmpty
+	private String senha;
+
+	@ManyToMany
+	@JoinTable(name = "usuario_perfil", joinColumns = {@JoinColumn(name = "usuario")}, inverseJoinColumns = {@JoinColumn(name = "perfil")})
+	private List<Perfil> perfis;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Perfil> getPerfis() {
+		return perfis;
+	}
+
+	public void setPerfis(List<Perfil> perfis) {
+		this.perfis = perfis;
+	}
+}
